a4194ccbe6461623943fab20b251025630f490f9
AxelKohler <Kohleraxel8@gmail.com>
2026-02-27 11:33:36 +0100

    Added a sentence in 2.

--- src/Main.java
+++ src/Main.java
@@ -28,7 +28,7 @@ public final class Main {
 
         System.out.println("Vor langer langer Zeit lebte einst ein kleines Maedchen, das alle ");
         System.out.println("Rotkaeppchen nannten, denn es trug immer ein feuerrotes Maentelchen.");
-        System.out.println();
+        System.out.println("Der Drache ist genau so Rot!");
 
         /*
          * Drittes Kapitel
